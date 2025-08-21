09572f6d608ad0a6de8bfa368aed085f20078faa
Std-Enigma <std.ilia.bahrebar@gmail.com>
2025-08-21 22:10:49 +0330

    feat(core): Create application's filesystem handler

---
+++ core/paths.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from appdirs import user_data_dir, user_log_dir
+
+APP_NAME = "EchoNet"
+APP_AUTHOR = "EchoTerm"
+
+# --- Base directories ---
+LOG_DIR = Path(user_log_dir(appname=APP_NAME, appauthor=APP_AUTHOR))
+DATA_DIR = Path(user_data_dir(appname=APP_NAME, appauthor=APP_AUTHOR))
+
+# Ensure directories exist
+for directory in (LOG_DIR, DATA_DIR):
+    directory.mkdir(parents=True, exist_ok=True)
+
+# --- File paths ---
+# Core files
+LOG_FILE = LOG_DIR / "echonet.log"
+
+# Data Files
+DNS_CONFIGS_DB = DATA_DIR / "database.db"
